0f731cbc4705ffebc0db4ee03745e8964e1b3113
Andreas Rozek <A.Rozek@gmx.de>
2024-03-13 17:41:49 +0100

    Update automerge-bundle.js

---
+++ src/automerge-bundle.js
@@ -0,0 +1,6 @@
+import { next }                           from "@automerge/automerge"
+import { IndexedDBStorageAdapter }        from "@automerge/automerge-repo-storage-indexeddb"
+import { BrowserWebSocketClientAdapter }  from "@automerge/automerge-repo-network-websocket"
+import { BroadcastChannelNetworkAdapter } from "@automerge/automerge-repo-network-broadcastchannel"
+
+export { next, IndexedDBStorageAdapter, BrowserWebSocketClientAdapter, BroadcastChannelNetworkAdapter }
