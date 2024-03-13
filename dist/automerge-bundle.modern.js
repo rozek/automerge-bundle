532bacc40108ebc338586209c383f80f91234927
Andreas Rozek <A.Rozek@gmx.de>
2024-03-13 18:11:53 +0100

    new build

---
+++ dist/automerge-bundle.modern.js
@@ -0,0 +1,2 @@
+export{next}from"@automerge/automerge";export{IndexedDBStorageAdapter}from"@automerge/automerge-repo-storage-indexeddb";export{BrowserWebSocketClientAdapter}from"@automerge/automerge-repo-network-websocket";export{BroadcastChannelNetworkAdapter}from"@automerge/automerge-repo-network-broadcastchannel";
+//# sourceMappingURL=automerge-bundle.modern.js.map
