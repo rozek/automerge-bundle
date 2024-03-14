81b537e4d822a5e67e2e9f7cd9de79343abd6184
Andreas Rozek <A.Rozek@gmx.de>
2024-03-14 10:34:47 +0100

    new build

--- dist/automerge-bundle.d.ts
+++
@@ -1,5 +0,0 @@
-import { next } from "@automerge/automerge";
-import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb";
-import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket";
-import { BroadcastChannelNetworkAdapter } from "@automerge/automerge-repo-network-broadcastchannel";
-export { next, IndexedDBStorageAdapter, BrowserWebSocketClientAdapter, BroadcastChannelNetworkAdapter };
--- dist/automerge-bundle.module.js
+++
@@ -1,2 +0,0 @@
-export{next}from"@automerge/automerge";export{IndexedDBStorageAdapter}from"@automerge/automerge-repo-storage-indexeddb";export{BrowserWebSocketClientAdapter}from"@automerge/automerge-repo-network-websocket";export{BroadcastChannelNetworkAdapter}from"@automerge/automerge-repo-network-broadcastchannel";
-//# sourceMappingURL=automerge-bundle.module.js.map
