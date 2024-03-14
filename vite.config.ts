5f5da5d1bfa1ccade059da838f9879d9b1d03ace
Andreas Rozek <A.Rozek@gmx.de>
2024-03-14 10:34:15 +0100

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import wasm from "vite-plugin-wasm"
+import topLevelAwait from "vite-plugin-top-level-await"
+
+export default defineConfig({
+  plugins: [topLevelAwait(), wasm()],
+
+  build: {  	
+  	rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  },
+
+  worker: {
+    format: "es",
+    plugins: [wasm()],
+  },
+})
